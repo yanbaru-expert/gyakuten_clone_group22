78cf9ade775c6919d5ea4279756a471510a47bc8
ko <kyokucho1989@gmail.com>
2020-06-19 21:13:45 +0900

    タスクNo.14 管理者画面の実装_修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,17 @@
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
 
-# テストユーザーが存在しないときだけ作成
+ADMINEMAIL = 'admin@example.com'
+ADMINPASS = 'password'
+#通常ユーザ： テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts 'ユーザーの初期データインポートに成功しました。'
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+#管理者： テストユーザーが存在しないときだけ作成
+AdminUser.find_or_create_by!(email: ADMINEMAIL) do |admin_user|
+  admin_user.password = ADMINPASSWORD
+  puts '管理者の初期データインポートに成功しました。'
+end
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
