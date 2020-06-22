ab9f8e984937d990f3429cc157a566769a398055
keisuke-333 <keikubo.999@gmail.com>
2020-06-22 22:07:45 +0900

    質問集の質問投稿ページを実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,19 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all.order(created_at: :desc)
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    Question.create(title: question_params[:title], detail: question_params[:detail])
+    redirect_to action: :index
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :detail)
+  end
 end
