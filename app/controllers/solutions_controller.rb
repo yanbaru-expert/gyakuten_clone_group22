db508b7ca665e39654521f09641b8dd0b85a3086
yuukii125 <y.kipro125@gmail.com>
2020-06-28 14:43:41 +0900

    solutions/createのリファクタリング

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,12 +1,12 @@
 class SolutionsController < ApplicationController
 
   def create
-    @solution = Solution.new(solution_params)
-    @question = @solution.question
-    @solutions = @question.solutions
+    @question = Question.find(params[:question_id])
+    @solution = @question.solutions.build(solution_params)
     if @solution.save
       redirect_to question_url(@solution.question_id), notice: "回答を投稿しました"
     else
+      @solutions = @question.solutions
       flash.now[:alert] = "回答を入力してください"
       render "questions/show"
     end
@@ -15,7 +15,7 @@ class SolutionsController < ApplicationController
   private
 
     def solution_params
-      params.permit(:answer, :question_id)
+      params.permit(:answer)
     end
   
 end
