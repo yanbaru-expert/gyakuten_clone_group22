ee60848d859af50112b456d14a555d13f38125cc
morita daiki <morita.daiki.works@gmail.com>
2020-07-04 15:41:31 +0900

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   root "movies#index"
 
   resources :aws_texts, only: [:index, :show]
+  resources :lines    , only: [:index, :show]
   resources :questions, only: [:index, :create, :show] do
   resources :solutions, only: [:create]
-  resources :lines    , only: [:index, :show]
   end
 end
